8ebc49e81cb112263ca0522023f741426d1ea25e
Truman329 <49828645+Truman329@users.noreply.github.com>
2019-04-24 20:46:01 -0400

    Add files via upload

---
+++ movement.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class movement : MonoBehaviour
+{
+    public GameObject player;
+    //public Vector3 teleport;
+    public float speed;
+    private Rigidbody2D rb;
+    //public Text countText;
+    //public Text winText;
+    private int count;
+    public Text countText;
+    public Text winText;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        //SetCountText();
+        //winText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        if (Input.GetKeyDown (KeyCode.W)) 
+        {
+        player.transform.position = player.transform.position + new Vector3(0f, 1f, 0f);
+        }
+        
+
+    }
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+        //float move = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(speed * move, rb.velocity.y);
+
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Cherry: " + count.ToString();
+        if (count >= 1)
+        {
+            winText.text = "You won";
+        }
+    }
+    
+
+}
+    
---
+++ randomcolor3.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+// when a button is click change the color to the random one
+public class randomcolor3 : MonoBehaviour 
+{
+	public Color color = new Color(0.2F, 0.3F, 0.4F, 0.5F);
+	//public GameObject background;
+	
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		color = new Color(Random.value, Random.value, Random.value, Random.value); // the color is changing to a different random color is every frame.
+
+		if (Input.GetKeyDown (KeyCode.Q)) 
+		{
+			gameObject.GetComponent<SpriteRenderer> ().color = color; // the color of the gameobject is changing its color to the random color.
+		}
+
+		if (Input.GetKeyDown (KeyCode.W)) 
+		{
+			gameObject.GetComponent<SpriteRenderer> ().color = color; // the color of the gameobject is changing its color to the random color.
+		}
+
+		if (Input.GetKeyDown (KeyCode.E)) 
+		{
+			gameObject.GetComponent<SpriteRenderer> ().color = color; // the color of the gameobject is changing its color to the random color.
+		}
+
+		if (Input.GetKeyDown (KeyCode.R)) 
+		{
+			gameObject.GetComponent<SpriteRenderer> ().color = color; // the color of the gameobject is changing its color to the random color.
+		}
+	}
+}
