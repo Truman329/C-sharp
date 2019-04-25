1c45aebbde2889b0a484834f0040cf798aef9fff
Truman329 <49828645+Truman329@users.noreply.github.com>
2019-04-24 20:50:37 -0400

    Update pickup.cs

--- pickup.cs
+++ pickup.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// This code is used on a objective and when a player touch the objective
+//The objective will disappear
 public class pickup : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -17,7 +18,7 @@ public class pickup : MonoBehaviour
     }
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Pick Up"))
+        if (other.gameObject.CompareTag("Object"))
         {
             other.gameObject.SetActive(false);
         }
